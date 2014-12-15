5cde07a9e82e97e97c7d51eedecd819257261ce6
sabres207 <sagesdoron@gmail.com>
2014-12-15 02:31:52 +0200

    new angular project generated by yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name holaApp
+ * @description
+ * # holaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('holaApp', [
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
